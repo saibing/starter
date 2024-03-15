c34a5701f41d79c82e9938ab5685b233392728ff
saibing <saibingdeng@gmail.com>
2024-03-15 10:03:43 +0800

    add vim plugins config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,4 +12,67 @@ return {
       git = { enable = true },
     },
   },
+
+  {
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		--build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
+
+	{ "kevinhwang91/nvim-bqf", lazy = false },
+	{ "sindrets/diffview.nvim", dependencies = { "nvim-lua/plenary.nvim" }, lazy = false },
+	{ "windwp/nvim-spectre", lazy = false },
+	--{ "github/copilot.vim", lazy = false},
+	{
+		"folke/trouble.nvim",
+		config = true,
+		event = { "CmdlineEnter" },
+	},
+	{
+		"kevinhwang91/nvim-hlslens",
+		config = true,
+		lazy = true,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		dependencies = { "mfussenegger/nvim-dap" },
+	},
+	{ "nvim-telescope/telescope-dap.nvim" },
+	{ "tpope/vim-surround" },
+	{ "tpope/vim-abolish" },
+	{ "chentoast/marks.nvim" },
+	{
+		"folke/todo-comments.nvim",
+		config = true,
+	},
+	--{
+	--	"akinsho/toggleterm.nvim",
+	--	version = "*",
+	--	config = true,
+	--},
+	{
+		"simrat39/symbols-outline.nvim",
+		config = true,
+	},
+	{ "duane9/nvim-rg" },
+	{
+		"simrat39/rust-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+		--ft = { "rust", "cargo" },
+	},
+	{ "dhruvasagar/vim-table-mode" },
+	{ "junegunn/vim-easy-align" },
 }
