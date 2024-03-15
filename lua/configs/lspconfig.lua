25ab7ad14f769e4994780c1f19cf10f75dd09638
saibing <saibingdeng@gmail.com>
2024-03-15 18:51:07 +0800

    Create lspconfig.lua

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,68 @@
+local configs = require("nvchad.configs.lspconfig")
+
+local on_attach = configs.on_attach
+local on_init = configs.on_init
+local capabilities = configs.capabilities
+
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+
+local servers = { "html", "cssls", "jsonls", "clangd", "rust_analyzer", "jdtls"}
+
+local lsp_option = {
+	on_attach = on_attach,
+  on_init = on_init,
+	capabilities = capabilities,
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup(lsp_option)
+end
+
+local gopls_settings = {
+	analyses = {
+		unusedparams = true,
+	},
+	staticcheck = true,
+	gofumpt = true,
+}
+
+gopls_settings["local"] = "huawei.com"
+
+local gopls_option = {
+	on_attach = on_attach,
+  on_init = on_init,
+	capabilities = capabilities,
+	cmd = { "gopls", "serve" },
+	filetypes = { "go", "gomod" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = gopls_settings,
+	},
+}
+lspconfig.gopls.setup(gopls_option)
+
+local denols_option = {
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+        root_dir = util.root_pattern("deno.json", "deno.jsonc"),
+}
+lspconfig.denols.setup(denols_option)
+
+local tsserver_option = {
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+        root_dir = util.root_pattern("package.json"),
+}
+lspconfig.tsserver.setup(tsserver_option)
+
+local rt = require("rust-tools")
+rt.setup({
+	server = {
+		capabilities = capabilities,
+    on_init = on_init,
+		on_attach = on_attach,
+	},
+})
