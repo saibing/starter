c0b3dabe5937b8c2249ac13ada0d449d44a89621
saibing <saibingdeng@gmail.com>
2024-03-15 22:02:04 +0800

    add teminal key map

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,13 @@ end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 
--- add custom map
+-- telescope keymap
 map("n", "<C-p>", "<cmd> Telescope find_files<CR>", { desc = "Find files" })
 map("n", "<C-d>", "<cmd> Telescope lsp_document_symbols<CR>", { desc = "Lsp document symbols" })
+
+-- terminal keymap
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "escape terminal mode" })
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "window left" })
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "window right" })
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "window down" })
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "windows up" })
