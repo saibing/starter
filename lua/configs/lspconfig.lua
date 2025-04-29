77c265a20be474b9c18d147c08aaec8274579b54
saibing <saibingdeng@gmail.com>
2025-04-29 21:40:34 +0800

    use vim.lsp.config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,63 +1,14 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = vim.lsp.config
-
--- EXAMPLE
-local servers = { "html", "cssls", "jsonls", "clangd", "rust_analyzer", "jdtls", "bashls", "pyright" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
--- gopls
-local gopls_settings = {
-  analyses = {
-    unusedparams = true,
-  },
-  staticcheck = true,
-  gofumpt = true,
-}
-
-gopls_settings["local"] = "huawei.com"
-
-local gopls_option = {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  cmd = { "gopls", "serve" },
-  filetypes = { "go", "gomod" },
-  settings = {
-    gopls = gopls_settings,
-  },
-}
-lspconfig.gopls.setup(gopls_option)
+local servers = { "html", "cssls", "jsonls", "gopls", "clangd", "rust_analyzer", "jdtls", "bashls", "pyright" }
 
 local ts_ls = os.getenv "TS_LS"
 
 if ts_ls == "ts_ls" then
-  lspconfig.ts_ls.setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+  table.insert(servers, "ts_ls")
 else
-  lspconfig.denols.setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+  table.insert(servers, "denols")
 end
+
+vim.lsp.enable(servers)
