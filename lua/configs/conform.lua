bc440c781723c01e56eea028d23005595fbfd1cd
saibing <saibingdeng@gmail.com>
2026-03-27 10:48:08 +0800

    add python ruff_foramt config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,7 +4,7 @@ local options = {
     css = { "prettierd", "prettier", stop_after_first = true },
     html = { "prettierd", "prettier", stop_after_first = true },
     -- Conform will run multiple formatters sequentially
-    python = { "ruff", stop_after_first = true },
+    python = { "ruff_format", stop_after_first = true },
     -- You can customize some of the format options for the filetype (:help conform.format)
     rust = { "rustfmt", lsp_format = "fallback" },
     -- Conform will run the first available formatter
@@ -16,7 +16,7 @@ local options = {
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_format = "fallback",
   },
 }
 
