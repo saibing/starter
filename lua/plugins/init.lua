1de4fd484b5a805d92d9d84e253730e1f8dcac9b
saibing <saibing@live.com>
2026-07-28 17:58:02 +0800

    add rest.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -355,4 +355,14 @@ return {
       words = { enabled = true },
     },
   },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    },
+  },
 }
